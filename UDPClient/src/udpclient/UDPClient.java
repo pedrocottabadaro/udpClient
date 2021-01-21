ba68022672e837d7f46b58eb2c80d2f5a53de9c0
Davi Rezende <davijf_404@hotmail.com>
2021-01-20 22:46:44 -0300

    davi: clean code e corrigindo detalhes

--- UDPClient/src/udpclient/UDPClient.java
+++ UDPClient/src/udpclient/UDPClient.java
@@ -1,59 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package udpclient;
+//@author Pedro Cotta Badaro - 201776014
+//@author Davi Rezende Domingues - 201765505ab
+import java.io.*;
+import java.net.*;
 
-/**
- *
- * @author pedro
- */
-import java.io.*; 
-import java.net.*; 
-  
-class UDPClient { 
-    
-    public static void main(String args[]) throws Exception 
-    { 
-    
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
-
-        DatagramSocket clientSocket = new DatagramSocket(); 
-
-        InetAddress IPAddress = InetAddress.getByName("192.168.0.131"); 
-        while(true){
-        byte[] sendData = new byte[1024]; 
-        byte[] receiveData = new byte[1024]; 
-
-        System.out.println("Enviar Msg:\n");
-        String sentence = inFromUser.readLine(); 
+class UDPClient {
+    public static void main(String args[]) throws Exception {
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("192.168.0.131");
         
-        sendData = sentence.getBytes();         
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
+        while (true) {
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
 
-        clientSocket.send(sendPacket); 
-        
-        if(sentence.contains("#")){
-            clientSocket.close(); 
-            System.exit(1);
+            System.out.println("INSERIR MENSAGEM: ");
+            String sentence = inFromUser.readLine();
+
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+
+            //VERIFICA SE EH PRA ENCERRAR
+            if (sentence.contains("#")) {
+                clientSocket.close();
+                System.exit(1);
+            }
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+
+            //VERIFICA SE EH PRA ENCERRAR
+            if (modifiedSentence.contains("#")) {
+                clientSocket.close();
+                System.exit(1);
+            }
+            //IMPRIME MENSAGEM QUE VEIO DO SERVIDOR
+            System.out.println("FROM SERVER: " + modifiedSentence);
         }
-
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
-
-        clientSocket.receive(receivePacket); 
-
-        String modifiedSentence = new String(receivePacket.getData()); 
-        
-        if(modifiedSentence.contains("#")){
-            clientSocket.close(); 
-            System.exit(1);
-        }
-
-        System.out.println("FROM SERVER:" + modifiedSentence); 
-        }
-    
-      
-      } 
-    
-} 
+    }
+}
--- UDPClient/src/udpclient/UDPServer.java
+++ UDPClient/src/udpclient/UDPServer.java
@@ -1,59 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package udpclient;
+//@author Pedro Cotta Badaro - 201776014
+//@author Davi Rezende Domingues - 201765505ab
+import java.io.*;
+import java.net.*;
 
-/**
- *
- * @author pedro
- */
-import java.io.*; 
-import java.net.*; 
-  
-class UDPServer { 
-  public static void main(String args[]) throws Exception 
-    { 
-  
-      DatagramSocket serverSocket = new DatagramSocket(9876); 
-  
-      byte[] receiveData = new byte[1024]; 
-      byte[] sendData  = new byte[1024]; 
+class UDPServer {
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData;
+        byte[] sendData = new byte[1024];
         System.out.println("SERVER PORT 9876");
-      while(true) 
-        { 
-            receiveData = new byte[1024]; 
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+        
+        while (true) {
+            receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
 
-            serverSocket.receive(receivePacket); 
-
-            String sentence = new String(receivePacket.getData()); 
+            String sentence = new String(receivePacket.getData());
             
-            if(sentence.contains("#")){
-              serverSocket.close();
-              System.exit(2);
-          }
+            //VERIFICA SE EH PRA ENCERRAR
+            if (sentence.contains("#")) {
+                serverSocket.close();
+                System.exit(2);
+            }
 
-            InetAddress IPAddress = receivePacket.getAddress(); 
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            
+            String capitalizedSentence = sentence;
+            System.out.println("FROM CLIENT: " + capitalizedSentence);
 
-            int port = receivePacket.getPort(); 
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("INSERIR MENSAGEM: ");
+            String dados = inFromServer.readLine();
+            sendData = dados.getBytes();
 
-            String capitalizedSentence = sentence; 
-            System.out.println("FROM CLIENT:"+capitalizedSentence);
-          
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in)); 
-            System.out.println("Enviar Msg:\n");
-            String dados = inFromServer.readLine(); 
-            sendData = dados.getBytes(); 
-  
-          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); 
-  
-          serverSocket.send(sendPacket); 
-          if(dados.contains("#")){
-              serverSocket.close();
-              System.exit(2);
-          }
-        } 
-    } 
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+            
+            //VERIFICA SE EH PRA ENCERRAR
+            if (dados.contains("#")) {
+                serverSocket.close();
+                System.exit(2);
+            }
+        }
+    }
 }
